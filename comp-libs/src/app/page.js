78b759da9596ced67e78962b0d0f66627228e8d1
myles-hash <mylesbillington@gmail.com>
2024-01-31 13:49:32 +0000

    ws comp

---
+++ comp-libs/src/app/page.js
@@ -0,0 +1,39 @@
+import CheckboxDemo from "@/comps/Checkbox";
+import DialogDemo from "@/comps/Dialogue";
+import PopoverDemo from "@/comps/PopOver";
+import SwitchDemo from "@/comps/Switch";
+import TooltipDemo from "@/comps/Tooltip";
+import TwitterDemo from "@/comps/Twitter";
+
+
+
+export default function Home() {
+  return (<>
+    <h1>Home</h1>
+    <br/>
+    <br/>
+    <br/>
+    <PopoverDemo />
+    <br/>
+    <br/>
+    <br/>
+    <CheckboxDemo />
+    <br/>
+    <br/>
+    <br/>
+    <SwitchDemo />
+    <br/>
+    <br/>
+    <br/>
+    <DialogDemo />
+    <br/>
+    <br/>
+    <br/>
+    <TooltipDemo />
+    <br/>
+    <br/>
+    <br/>
+    <TwitterDemo />
+    </>
+  )
+}
---
+++ comp-libs/src/comps/Twitter.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { FaTwitter } from "react-icons/fa";
+
+export default function TwitterDemo() {
+    return (
+        <div>
+            Icon: <FaTwitter />
+        </div>
+    )
+}
